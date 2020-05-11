beea9f34af41f2843790a8558d0d0bc7172e1ad9
Abin Abraham <abraham.abin13@gmail.com>
2020-05-11 14:17:01 -0500

    update demo.sh

--- demo.sh
+++ demo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 
-outputpath="/gpfs51/dors2/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec"
+outputpath=$(pwd)
 python run_evo_sig.py demo demo/bmi_small.test $outputpath
