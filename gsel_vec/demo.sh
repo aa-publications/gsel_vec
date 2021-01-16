64c34e958f65c1c3dd07a22a9483d379aef14cb7
Abin Abraham <abraham.abin13@gmail.com>
2021-01-15 22:43:15 -0600

    moved all scripts to inner dir

--- gsel_vec/demo.sh
+++ gsel_vec/demo.sh
@@ -3,7 +3,9 @@
 
 outputpath=$(pwd)
 input_gwas="/dors/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec/test/input_data/bmi_40k.txt"
+# input_gwas="/dors/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec/test/input_data/bmi_small.test"
 
 # input_gwas="/dors/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec/test/input_data/formatted_Bmi.giant-ukbb.meta-analysis.combined.23May2018.HapMap2_only.txt"
 
-ipython --pdb -c "%run  run_evo_sig.py bmi40k ${input_gwas} ${outputpath}"
+# ipython --pdb -c "%run  run_evo_sig.py bmi40k ${input_gwas} ${outputpath}"
+ipython --pdb -c "%run  run_evo_sig.py bmi_small ${input_gwas} ${outputpath}"
