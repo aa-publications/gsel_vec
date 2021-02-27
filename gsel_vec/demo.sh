0a93b64524b6b6922b8df65fc484a48e6e020c35
Abin Abraham <abraham.abin13@gmail.com>
2021-02-27 17:58:48 -0600

    updated demo script

--- gsel_vec/demo.sh
+++ gsel_vec/demo.sh
@@ -3,9 +3,8 @@
 
 outputpath=$(pwd)
 
-input_gwas="/dors/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec/gsel_vec/demo/input_data/bmi_40k.txt"
+input_gwas="demo/input_data/bmi_40k.txt"
 # input_gwas="/dors/capra_lab/projects/gwas_allele_age_evolution/scripts/pipeline/dev/gsel_vec/gsel_vec/demo/input_data/bmi_small.test"
 
 
-# ipython --pdb -c "%run  run_evo_sig.py bmi40k ${input_gwas} ${outputpath}"
-ipython --pdb -c "%run  run_evo_sig_test.py bmi_small ${input_gwas} ${outputpath}"
+ipython --pdb -c "%run  run_evo_sig_test.py demo_test ${input_gwas} ${outputpath}"
