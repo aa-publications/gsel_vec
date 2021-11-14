178017cf9cdb7cc2460417fa8b7bb5cf80ceece0
Abin Abraham <abraham.abin13@gmail.com>
2021-11-14 14:06:18 -0600

    updated download script to unzip 1kg and anno dict files

--- download_data.sh
+++ download_data.sh
@@ -15,7 +15,7 @@ OUTPUT_DIR=${PWD}"/gsel_vec/data"
 wget -O ${OUTPUT_DIR}"/anno_dict/anno_dict.zip" $ANNO_DICT
 unzip ${OUTPUT_DIR}"/anno_dict/anno_dict.zip" -d ${OUTPUT_DIR}/"anno_dict"
 rm ${OUTPUT_DIR}"/anno_dict/anno_dict.zip"
-
+gunzip ${OUTPUT_DIR}/anno_dict/*.gz
 
 
 output_name=${OUTPUT_DIR}"/snpsnap_database/snpsnap_database_ld0.9.tab.gz"
@@ -25,6 +25,7 @@ wget -O $output_name $SNPSNAP -d ${OUTPUT_DIR}/"snpsnap_database"
 output_name=${OUTPUT_DIR}"/1kg/1kg.zip"
 wget -O $output_name $KG_DICT
 unzip $output_name -d ${OUTPUT_DIR}/"1kg"
+gunzip ${OUTPUT_DIR}/1kg/*.gz
 rm $output_name
 
 
