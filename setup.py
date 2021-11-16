406d888897f51584054bc281fff4874ed2aa16e6
Abin Abraham <abraham.abin13@gmail.com>
2021-11-15 22:34:58 -0600

    modified setupt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     license="MIT",
     description="Detect signatures of evolution from GWAS data.",
     version="v0.0.1",
-    long_description="tbd",
+    long_description="long descrip",
     url="https://github.com/abraham-abin13/gsel_vec",
     scripts=["gsel_vec/run_evo_sig.py"],
     packages=setuptools.find_packages(),
